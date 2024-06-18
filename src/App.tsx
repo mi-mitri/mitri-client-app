6359c30a21a2534fd7bdfb134683d06cfbc872b1
Mikhail T. <m.tridchikov@gmail.com>
2024-06-18 22:09:44 +0300

    Telegram API connect

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,83 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Navbar from './components/Navbar';
 import Header from './components/Header';
 import Workspace from './components/Workspace';
 import Upgrades from './components/Upgrades';
 import Friends from './components/Friends';
 import Wallet from './components/Wallet';
-import './components/styles/App.scss';
+import { AppProvider } from './context/AppContext';
+import './styles/App.scss';
 
 const App: React.FC = () => {
-  const [page, setPage] = useState<string>('workspace');
+  const [page, setPage] = useState('workspace');
+  const [username, setUsername] = useState('');
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
 
-  const renderPage = () => {
-    switch (page) {
-      case 'workspace':
-        return <Workspace />;
-      case 'upgrades':
-        return <Upgrades />;
-      case 'friends':
-        return <Friends />;
-      case 'wallet':
-        return <Wallet />;
-      default:
-        return <Workspace />;
+  useEffect(() => {
+    const telegram = (window as any).Telegram.WebApp;
+    if (telegram && telegram.initDataUnsafe) {
+      const { username, first_name, last_name } = telegram.initDataUnsafe.user || {};
+      setUsername(username || '');
+      setFirstName(first_name || '');
+      setLastName(last_name || '');
     }
-  };
+  }, []);
 
   return (
-    <div className="app">
-      <Header />
-      <div className="content">
-        {renderPage()}
+    <AppProvider>
+      <div className="App">
+        <Header username={username} firstName={firstName} lastName={lastName} />
+        {page === 'workspace' && <Workspace />}
+        {page === 'upgrades' && <Upgrades />}
+        {page === 'friends' && <Friends />}
+        {page === 'wallet' && <Wallet />}
+        <Navbar setPage={setPage} />
       </div>
-      <Navbar setPage={setPage} />
-    </div>
+    </AppProvider>
   );
 };
 
 export default App;
+
+
+
+// import React, { useState } from 'react';
+// import Navbar from './components/Navbar';
+// import Header from './components/Header';
+// import Workspace from './components/Workspace';
+// import Upgrades from './components/Upgrades';
+// import Friends from './components/Friends';
+// import Wallet from './components/Wallet';
+// import './components/styles/App.scss';
+
+// const App: React.FC = () => {
+//   const [page, setPage] = useState<string>('workspace');
+
+//   const renderPage = () => {
+//     switch (page) {
+//       case 'workspace':
+//         return <Workspace />;
+//       case 'upgrades':
+//         return <Upgrades />;
+//       case 'friends':
+//         return <Friends />;
+//       case 'wallet':
+//         return <Wallet />;
+//       default:
+//         return <Workspace />;
+//     }
+//   };
+
+//   return (
+//     <div className="app">
+//       <Header />
+//       <div className="content">
+//         {renderPage()}
+//       </div>
+//       <Navbar setPage={setPage} />
+//     </div>
+//   );
+// };
+
+// export default App;
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,17 +2,48 @@ import React from 'react';
 import { useAppContext } from '../context/AppContext';
 import './styles/components/Header.scss';
 
-const Header: React.FC = () => {
+interface HeaderProps {
+  username: string;
+  firstName: string;
+  lastName: string;
+}
+
+const Header: React.FC<HeaderProps> = ({ username, firstName, lastName }) => {
   const { coins, coinRate } = useAppContext();
 
   return (
-    <header className="header">
-      <div className="coin-info">
-        <span>Coins: {Math.floor(coins)}</span>
-        <span>Coins per hour: {coinRate}</span>
+    <div className="header">
+      <div className="user-info">
+        <span className="username">{username}</span>
+        <span className="full-name">{firstName} {lastName}</span>
       </div>
-    </header>
+      <div className="coin-info">
+        <span className="coin-count">{coins} Coins</span>
+        <span className="coin-rate">{coinRate} Coins/hr</span>
+      </div>
+    </div>
   );
 };
 
 export default Header;
+
+
+
+// import React from 'react';
+// import { useAppContext } from '../context/AppContext';
+// import './styles/components/Header.scss';
+
+// const Header: React.FC = () => {
+//   const { coins, coinRate } = useAppContext();
+
+//   return (
+//     <header className="header">
+//       <div className="coin-info">
+//         <span>Coins: {Math.floor(coins)}</span>
+//         <span>Coins per hour: {coinRate}</span>
+//       </div>
+//     </header>
+//   );
+// };
+
+// export default Header;
