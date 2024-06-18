332b88b78ad62f8a326eb01dd0a9fe252e28d9f0
Mikhail T. <m.tridchikov@gmail.com>
2024-06-19 00:29:01 +0300

    Backup way mistakes with DB

--- src/App.tsx
+++ src/App.tsx
@@ -18,20 +18,10 @@ const App: React.FC = () => {
     // @ts-ignore
     const telegram = window.Telegram.WebApp;
     if (telegram && telegram.initDataUnsafe) {
-      const { username, first_name, last_name, id } = telegram.initDataUnsafe.user || {};
+      const { username, first_name, last_name } = telegram.initDataUnsafe.user || {};
       setUsername(username || '');
       setFirstName(first_name || '');
       setLastName(last_name || '');
-
-      // Send user data to the server
-      fetch(`http://localhost:3000/get-user-data/${id}`)
-        .then(response => response.json())
-        .then(data => {
-          console.log('User data:', data);
-        })
-        .catch(error => {
-          console.error('Error fetching user data:', error);
-        });
     }
   }, []);
 
@@ -50,38 +40,3 @@ const App: React.FC = () => {
 };
 
 export default App;
-
-// const App: React.FC = () => {
-//   const [page, setPage] = useState('workspace');
-//   const [username, setUsername] = useState('');
-//   const [firstName, setFirstName] = useState('');
-//   const [lastName, setLastName] = useState('');
-
-//   useEffect(() => {
-//     // @ts-ignore
-//     const telegram = window.Telegram.WebApp;
-//     if (telegram && telegram.initDataUnsafe) {
-//       const { username, first_name, last_name } = telegram.initDataUnsafe.user || {};
-//       setUsername(username || '');
-//       setFirstName(first_name || '');
-//       setLastName(last_name || '');
-//     }
-//   }, []);
-
-//   return (
-//     <AppProvider>
-//       <div className="App">
-//         <Header username={username} firstName={firstName} lastName={lastName} />
-//         {page === 'workspace' && <Workspace />}
-//         {page === 'upgrades' && <Upgrades />}
-//         {page === 'friends' && <Friends />}
-//         {page === 'wallet' && <Wallet />}
-//         <Navbar setPage={setPage} />
-//       </div>
-//     </AppProvider>
-//   );
-// };
-
-// export default App;
-
-
