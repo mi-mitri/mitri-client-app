f3bd82a7ae921b21fbe2a42756f5f703d3a09ba6
Mikhail T. <m.tridchikov@gmail.com>
2024-06-18 23:36:54 +0300

    DB connecting try

--- src/App.tsx
+++ src/App.tsx
@@ -18,10 +18,20 @@ const App: React.FC = () => {
     // @ts-ignore
     const telegram = window.Telegram.WebApp;
     if (telegram && telegram.initDataUnsafe) {
-      const { username, first_name, last_name } = telegram.initDataUnsafe.user || {};
+      const { username, first_name, last_name, id } = telegram.initDataUnsafe.user || {};
       setUsername(username || '');
       setFirstName(first_name || '');
       setLastName(last_name || '');
+
+      // Send user data to the server
+      fetch(`http://localhost:3000/get-user-data/${id}`)
+        .then(response => response.json())
+        .then(data => {
+          console.log('User data:', data);
+        })
+        .catch(error => {
+          console.error('Error fetching user data:', error);
+        });
     }
   }, []);
 
@@ -41,43 +51,37 @@ const App: React.FC = () => {
 
 export default App;
 
-
-// import React, { useState } from 'react';
-// import Navbar from './components/Navbar';
-// import Header from './components/Header';
-// import Workspace from './components/Workspace';
-// import Upgrades from './components/Upgrades';
-// import Friends from './components/Friends';
-// import Wallet from './components/Wallet';
-// import './components/styles/App.scss';
-
 // const App: React.FC = () => {
-//   const [page, setPage] = useState<string>('workspace');
+//   const [page, setPage] = useState('workspace');
+//   const [username, setUsername] = useState('');
+//   const [firstName, setFirstName] = useState('');
+//   const [lastName, setLastName] = useState('');
 
-//   const renderPage = () => {
-//     switch (page) {
-//       case 'workspace':
-//         return <Workspace />;
-//       case 'upgrades':
-//         return <Upgrades />;
-//       case 'friends':
-//         return <Friends />;
-//       case 'wallet':
-//         return <Wallet />;
-//       default:
-//         return <Workspace />;
+//   useEffect(() => {
+//     // @ts-ignore
+//     const telegram = window.Telegram.WebApp;
+//     if (telegram && telegram.initDataUnsafe) {
+//       const { username, first_name, last_name } = telegram.initDataUnsafe.user || {};
+//       setUsername(username || '');
+//       setFirstName(first_name || '');
+//       setLastName(last_name || '');
 //     }
-//   };
+//   }, []);
 
 //   return (
-//     <div className="app">
-//       <Header />
-//       <div className="content">
-//         {renderPage()}
+//     <AppProvider>
+//       <div className="App">
+//         <Header username={username} firstName={firstName} lastName={lastName} />
+//         {page === 'workspace' && <Workspace />}
+//         {page === 'upgrades' && <Upgrades />}
+//         {page === 'friends' && <Friends />}
+//         {page === 'wallet' && <Wallet />}
+//         <Navbar setPage={setPage} />
 //       </div>
-//       <Navbar setPage={setPage} />
-//     </div>
+//     </AppProvider>
 //   );
 // };
 
 // export default App;
+
+
